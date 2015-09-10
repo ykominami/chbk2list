33085d03d9a0e0f2849cde4c3c8f3b7163253fc0
yasuo kominami <ykominami@gmail.com>
2015-09-11 00:07:22 +0900

    remove unnecessary files

--- main3.rb
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-require 'bookmark3'
-
-# パラメーターの読み込み
-filename = ARGV[0]
-
-parser = Bk::BookmarkHtmlParser.new( filename )
-bookmark = parser.parse
-bookmark.collect_category_x
