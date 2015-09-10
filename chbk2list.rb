57c8a953d5926975701ded2fffb4eae4833199ca
yasuo kominami <ykominami@gmail.com>
2015-09-10 23:32:35 +0900

    modify by ancestors method and rename chbk2list2.rb to chbe2list.rb

--- chbk2list.rb
+++ chbk2list.rb
@@ -1,187 +1,204 @@
 # -*- coding: utf-8 -*-
-require "pathname"
-require "nokogiri"
+require 'pathname'
+require 'nokogiri'
 
 class Chbk2list
-  class Node
-    def initialize(name)
-      @name = name
-    end
-  end
-
   def initialize(fname)
     @fname = fname
-    @obj_hs = {}
-    @body = nil
+    @item_hs = {}
+    @dl_hs = {}
   end
 
-  def get_top( item , level )
-    puts "----"
-    puts item.class
-    puts item.object_id
-    puts item.name
-    puts level
-    @obj_hs[item] ||= item.name
-    
-    if item.name == "body"
-      @body ||= item
-      top_level = level
-    else
-      top_level = get_top( item.parent , level + 1 ) 
-    end
-    top_level
-  end
-
-  def get_category_list2
-    @cnt = 3
-    File.open( @fname ){ |file|
-      doc = Nokogiri::HTML::Document.parse(file)
-      doc.search("a").inject([nil,{}]){|container, t|
-        top_level = get_top( t , 1 )
-        puts "top_level=#{top_level}"
-        puts ""
-        @cnt -= 1
-        exit if @cnt <= 0
-#
-        container
-      }
+  def register_hier_item( item )
+    rev_hier = item.ancestors()
+    rev_hier.pop
+    rev_hier.pop
+    rev_hier.pop
+    # rev_hierを逆順にして、itemを最後尾に追加する
+    # ancestors()の返値であるNokogiri::XML::NodeSetがpush, unshiftを
+    # サポートしていないため、一旦Arrayに詰め込んでから、itemを追加
+    hier = rev_hier.reduce([]){ |s,x|  s.unshift(x) }
+    hier.push( item )
+    hier.reduce([]){ |s,x|
+      register_item( x , nil )
+      s.push( @item_hs[x] )
+      @item_hs[x][:hier] = s[0 , s.size]
+      s
     }
   end
 
-  def inspect_item( item , n )
-    if item
-      case item[n].name
-      when "a"
-        puts "a"
-        puts item[n].text.strip
-      when "p"
-        puts "p"
-#        puts item[n+1].text.strip
-        inspect_item( item , n + 1 )
-      when "dt"
-        puts "dt"
-        inspect_item( item[n].children , 0 )
-      when "dl"
-        puts "dl"
-        inspect_item( item[n].children , 0 )
-      when "h3"
-        puts "h3|#{item[n].text.strip}"
-      else
-        puts "ELSE|#{item[n].name}"
+  def register_item( item , title = nil )
+    unless @item_hs[item]
+      @item_hs[item] = {
+        :name => item.name ,
+        :title => nil,
+        :item => item,
+        :hier => [],
+        :call_count => -1,
+      }
+    else
+      unless @item_hs[item][:name]
+        @item_hs[item][:name] = item.name
+      end
+      unless @item_hs[item][:item]
+        @item_hs[item][:item] = item
+      end
+      unless @item_hs[item][:hier]
+        @item_hs[item][:hier] = []
       end
     end
+    if title
+      @item_hs[item][:title] = title
+    end
+
+    if @item_hs[item][:title]
+      #
+    end
+    @item_hs[item][:call_count] += 1
   end
 
-  def inspect_parent( node )
-    puts "************"
-    p "node.name=#{node.name}"
-#    p "node.children.first.name=#{node.children.first.name}"
-#    puts node.children.first.text.strip if node.children.first.name == "a"
-    inspect_item( node.children , 0 )
+  def inspect_ancestors( item )
+    list = item.ancestors().map{ |x| "#{x.name}(#{x.object_id})<#{@item_hs[x] ? @item_hs[x][:title] : "nil"}>" }
+    list.pop
+    list.pop
+    list.pop
+    list.reverse.reduce{ |s,x| s + '/' + x  }
+  end
+  
+  def get_dl_belong_to( item )
+    # ancestors()は昇順の配列で返すので、最初に見つかったdlが最も近いdlになる
+    item.ancestors().find{ |x| x.name == "dl" }
+  end
 
-    p "parent.name=#{node.parent.name}"
-    ary = []
-    parent = nil
+  def assign_name_to_ancestor_h1(doc)
+    x =doc.search("h1").first
+    body_title = x.text.strip
+    parent = x.parent
     
-    if node and node.class == Nokogiri::XML::Element
-      parent = node.parent 
-      if parent.name != "body"
-        parent.children.each do |it|
-          break if it == node
-          if it.name =~ /dt/i
-            text = it.children.first.text.strip
-            puts "text=#{text}"
-#            ary << it.text.strip 
-            ary << text
-          end
-        end
-      else
-        parent = nil
-      end
-    end
-    [parent , ary.last]
+    dl = parent.children.find{ |x| x.name == "dl" }
+    register_hier_item( dl )
+    register_item( dl , body_title )
   end
 
-  def get_category( category_hs , node )
-    ary = []
-    hs = {}
+  def assign_name_to_ancestor_h3( doc , state = :second )
+    doc.search( "h3" ).each do |x|
+      text = x.text.strip
+      # parentはdt
+      parent = x.parent
+      if state != :second
+        register_hier_item( parent )
+        register_item( parent , text )
+      end
 
-    while( node )
-      node , element = inspect_parent( node )
-      if element
-        saved_ary = category_hs[ element ]
-        unless saved_ary
-          hs.each_value do |v|
-            v.unshift( element )
-          end
-          hs[ element ] = [ element ]
-          ary.unshift( element ) 
+      dl = get_dl_belong_to( parent )
+      if @item_hs[dl][:title] == nil
+        register_item( dl , @dl_hs[ dl.parent ][:text] ) if @dl_hs[ dl.parent ]
+        @dl_hs.delete(dl.parent)
+      elsif @item_hs[dl][:title].strip.size == 0
+        register_item( dl , @dl_hs[ dl.parent ][:text] ) if @dl_hs[ dl.parent ]
+        @dl_hs.delete(dl.parent)
+      else
+        if @dl_hs[dl] == nil
+          @dl_hs[ dl ] = { :child => parent, :text => text }
         else
-          hs.each do |k,v|
-            category_hs[k] = saved_ary + v
-          end
-          ary.unshift( save_ary )
+          @dl_hs[ dl ][:text] = text
+        end
+      end
+    end
+  end
+
+  def assign_name_to_ancestor_a_stage1(doc )
+    doc.search( "a" ).each do |x|
+      text = x.text.strip
+      # parentはdt
+      parent = x.parent
+      register_hier_item( parent )
+      register_item( parent, text )
+    end
+  end
+
+  def assign_name_to_ancestor_a_stage2(doc )
+    doc.search( "a" ).each do |x|
+      text = x.text.strip
+      # parentはdt
+      parent = x.parent
+
+      dl = get_dl_belong_to( parent )
+      dl_parent = dl.parent
+      idx = dl_parent.children.index(dl)
+      # dlの直前のdtを得る
+      step = 1
+      while true
+        dt = dl_parent.children[ idx - step ]
+        break if dt.name == "dt"
+        step += 1
+        if idx < step
+          dt = nil
           break
         end
       end
+      if @item_hs[dl] == nil
+        register_hier_item( dl )
+        register_item( dl , @item_hs[dt][:title] )
+      elsif @item_hs[dl][:title] == nil
+        @item_hs[dl][:title] = @item_hs[dt][:title]
+      end
     end
+  end  
 
-    ary
-  end
+  def listup_a(doc)
+    doc.search("a").each do |t|
+      str = ""
+      parent = t.parent
+      list = @item_hs[parent][:hier]
 
-  def get_list
-    File.open( @fname ){ |file|
-      doc = Nokogiri::HTML::Document.parse(file)
-      doc.search("a").inject([nil,{}]){|container, t|
-        prev_category_ary = container[0]
-        category_hs = container[1]
-        
-        category_ary = get_category( category_hs , t.parent )
-        category_ary.shift
-        if !prev_category_ary or !(prev_category_ary == category_ary)
-          puts ""
-          puts category_ary.join("/")
-          puts ""
-          prev_category_ary = category_ary
+#      puts inspect_ancestors( t )
+      
+      if !list or list.size == 0
+        register_hier_item(parent)
+        list = @item_hs[parent][:hier]
+      end
+      if list.size > 0
+        idx = list.find_index{ |x| x[:name] == "dt" }
+        if idx 
+#          listx = list[0, idx - 1 ]
+          listx = list[0, idx ]
+          if listx
+            ax = listx.collect{ |y| y[:title] }
+            str = ax.reduce(""){ |s,x| s + '/' + ( x != nil ? x : "") }
+            puts "#{str},#{t.text.strip},#{t['href']}\n"
+          end
         end
-        puts "-- #{t.text}"
-        puts t[:href]
-        
-        container[0] = prev_category_ary 
-        container
-      }
-    }
+      else
+        #
+      end
+    end
   end
 
   def get_category_list
-    @cnt = 20
     File.open( @fname ){ |file|
       doc = Nokogiri::HTML::Document.parse(file)
-      doc.search("a").inject([nil,{}]){|container, t|
-        prev_category_ary = container[0]
-        category_hs = container[1]
-        
-        category_ary = get_category( category_hs , t.parent )
-        category_ary.shift
-        if !prev_category_ary or !(prev_category_ary == category_ary)
-          puts category_ary.join("/")
-#
-          p category_ary
-          @cnt -= 1
-          exit if @cnt <= 0
-#
-          prev_category_ary = category_ary
-        end
-        container[0] = prev_category_ary 
-        container
-      }
+      assign_name_to_ancestor_h1( doc )
+      assign_name_to_ancestor_h3( doc , :first )
+      assign_name_to_ancestor_a_stage1(doc )
+
+      assign_name_to_ancestor_h3( doc , :second )
+      assign_name_to_ancestor_a_stage2(doc )
+
+#      print_ancestor_of_h3( doc )
+
+      listup_a(doc)
     }
+  end
+
+  def print_ancestor_of_h3( doc )
+    doc.search( "h3" ).each do |x|
+      puts inspect_ancestors( x )
+    end
   end
 end
 
 fname = ARGV[0]
 
-#str = Pathname( fname ).expand_path.read
 c2l = Chbk2list.new( fname )
-#c2l.get_category_list
-c2l.get_category_list2
+c2l.get_category_list
--- chbk2list2.rb
+++ chbk2list2.rb
@@ -1,416 +1,204 @@
 # -*- coding: utf-8 -*-
 require 'pathname'
 require 'nokogiri'
-#require 'minitest/autorun'
 
 class Chbk2list
-  class Node
-    attr_accessor :obj , :parent , :prev , :next , :child, :category, :name ,:youngest
-
-    def initialize(name)
-      @name = name
-    end
-  end
-
   def initialize(fname)
     @fname = fname
-    @node_hs = {}
-    @body = nil
-
-    @tag_list = []
     @item_hs = {}
     @dl_hs = {}
-    @dt_hs = {}
   end
 
-  def item_n_root_root(root)
-    unless @node_hs[root]
-      name = root.search("h1").first.text.strip
-      @node_hs[:ROOT] = @node_hs[root] = Node.new(name)
-    end
-
-    @node_hs[root]
+  def register_hier_item( item )
+    rev_hier = item.ancestors()
+    rev_hier.pop
+    rev_hier.pop
+    rev_hier.pop
+    # rev_hierを逆順にして、itemを最後尾に追加する
+    # ancestors()の返値であるNokogiri::XML::NodeSetがpush, unshiftを
+    # サポートしていないため、一旦Arrayに詰め込んでから、itemを追加
+    hier = rev_hier.reduce([]){ |s,x|  s.unshift(x) }
+    hier.push( item )
+    hier.reduce([]){ |s,x|
+      register_item( x , nil )
+      s.push( @item_hs[x] )
+      @item_hs[x][:hier] = s[0 , s.size]
+      s
+    }
   end
 
-  def item_n_sub_x( item , parent)
-    raise unless @node_hs[item]
-    raise if @node_hs[item].next
-    raise if @node_hs[item].prev
-
-    status = :NORMAL
-    if @node_hs[parent]
-      if @node_hs[parent].child
-        if @node_hs[parent].youngest
-          # parentのノードがchild, youngestを持つ（ノーマル）
-        else
-          # parentのノードがchildのみを持っている。youngestが同じノードを指すようにする
-#          raise if @node_hs[parent].child.next
-          raise if @node_hs[parent].child.prev
-          @node_hs[parent].youngest = @node_hs[parent].child
-        end
-      else
-        if @node_hs[parent].youngest
-          # parentのノードがyoungestのみを持っている。childが同じノードを指すようにする
-          raise if @node_hs[parent].youngest.prev
-          raise if @node_hs[parent].youngest.next
-          @node_hs[parent].child = @node_hs[parent].youngest
-        else
-          # @node_hs[parent]が孤立している
-          status = :ALONE
-        end
-      end
+  def register_item( item , title = nil )
+    unless @item_hs[item]
+      @item_hs[item] = {
+        :name => item.name ,
+        :title => nil,
+        :item => item,
+        :hier => [],
+        :call_count => -1,
+      }
     else
-      # parentのノードが存在しないため、parentのノードを生成して、child, youngestがitemのノードを指すようにする。
-      @node_hs[parent] = item_n(parent)
-      status = :ALONE
+      unless @item_hs[item][:name]
+        @item_hs[item][:name] = item.name
+      end
+      unless @item_hs[item][:item]
+        @item_hs[item][:item] = item
+      end
+      unless @item_hs[item][:hier]
+        @item_hs[item][:hier] = []
+      end
+    end
+    if title
+      @item_hs[item][:title] = title
     end
 
-    case status
-    when :NORMAL
-      @node_hs[parent].youngest.next = @node_hs[item]
-      @node_hs[item].prev = @node_hs[parent].youngest
-      @node_hs[parent].youngest = @node_hs[item]
-    when :ALONE
-      @node_hs[parent].child = @node_hs[item]
-      @node_hs[parent].youngest = @node_hs[item]
-    else
-      raise
+    if @item_hs[item][:title]
+      #
     end
-
-    if @node_hs[parent].youngest.next
-      raise 
-    end
-
-    @node_hs[item].parent = @node_hs[parent]
-    @node_hs[item].category = [@node_hs[parent].category , @node_hs[item].name].join('/')
+    @item_hs[item][:call_count] += 1
   end
 
-  def item_n_sub( item )
-    parent = item.parent
-
-    if @node_hs[parent]
-      if @node_hs[parent].child
-#        raise if @node_hs[parent].child.next
-        raise if @node_hs[parent].child.prev
-      end
-    end
-
-    # itemは<dl>要素または<dt>要素
-    case item.name.downcase
-    when "dl"
-      # <dl>の直前の<dt>の直下の<H3>のテキストから<dl>に対応する1個のNodeを作る
-      # parentが存在しなければならない
-      raise unless parent
-
-      case parent.name.downcase 
-      when "dl"
-        dt = parent.children[ parent.children.index(item) - 1 ]
-        name = dt.search("h3").first.text.strip
-        @node_hs[item] = Node.new(name)
-        
-        item_n_sub_x( item , parent)
-
-      when "body"
-        name = parent.search("h1").first.text.strip
-        @node_hs[item] = Node.new(name)
-        item_n_root_root(parent)
-        item_n_sub_x( item , parent)
-
-      else
-        # <dl> , <body>以外であれば例外を投げる
-        raise 
-      end
-    when "dt"
-      unless parent
-        raise
-      end
-      unless @node_hs[parent]
-        raise
-      end
-      raise
-
-    when "body"
-      # Nodeの木のルートにする
-      item_n_root_root(item)
-    else
-      # 上記以外のパターンであれば例外を投げる
-      raise
-    end
-
-    raise unless @node_hs[item]
-
-    @node_hs[item]
-  end
-
-  def item_n( item )
-    # itemが存在しなければならない
-    raise unless item
-
-    ret = @node_hs[item] != nil ? @node_hs[item] : item_n_sub( item )
-
-    ret
-  end
-
-  def item_a( item )
-    # itemは<a>要素
-    # item.parentは<dt>要素
-    parent = item.parent
-    # ここで生成するNode(またこのNodeは葉である)はitemとitem.parentに対応する
-    @node_hs[parent] = @node_hs[item] = Node.new( item.text.strip )
-
-    # parent.parentは<dt>要素あるいは<dl>要素
-
-    # parent.parentが初めて現れた場合は、item_nメソッドで対応するNodeを作成
-    unless @node_hs[parent.parent]
-      @node_hs[parent.parent] = item_n( parent.parent )
-    end
-
-    case parent.parent.name 
-    when "dl","body"
-      # Nodeの親子関係を設定
-      @node_hs[parent].parent = @node_hs[parent.parent]
-      unless @node_hs[parent.parent].child
-        @node_hs[parent.parent].child = @node_hs[parent]
-      end
-
-      unless @node_hs[parent.parent].youngest
-        @node_hs[parent.parent].child = @node_hs[parent]
-      else
-        raise if @node_hs[parent.parent].youngest.next
-        @node_hs[parent.parent].youngest.next = @node_hs[parent]
-        @node_hs[parent].prev = @node_hs[parent.parent].youngest
-        @node_hs[parent.parent].youngest = @node_hs[parent]
-      end
-      @node_hs[parent].parent = @node_hs[parent.parent] 
-
-      raise if @node_hs[parent].category
-      @node_hs[parent].category = @node_hs[parent.parent].category
-#
-    when "dt"
-      # Nodeの前後関係を設定
-      raise if @node_hs[parent.parent].next
-      @node_hs[parent.parent].next = @node_hs[parent]
-      @node_hs[parent].prev = @node_hs[parent.parent]
-      @node_hs[parent].parent = @node_hs[parent.parent].parent
-      @node_hs[parent].category = @node_hs[parent.parent].category
-    else
-      raise
-    end
-
-    @node_hs[item]
-  end
-
-  def get_top( item )
-    if item.name != "body"
-      @tag_list.unshift( item )
-      get_top( item.parent )
-    end
-  end
-
-  def item_dt( item )
-    ret = nil
-    ch = item.children[0]
-    case ch.name
-    when /H3/i
-#      puts "item_dt H3"
-      ret = ch.text
-    when /A/i
-#      puts "item_dt A"
-      ret = ch.text
-    else
-      puts "item_dt ELSE #{ch.name}"
-    end
-#    puts "item_dt ret=#{ret}"
-    
-    ret
+  def inspect_ancestors( item )
+    list = item.ancestors().map{ |x| "#{x.name}(#{x.object_id})<#{@item_hs[x] ? @item_hs[x][:title] : "nil"}>" }
+    list.pop
+    list.pop
+    list.pop
+    list.reverse.reduce{ |s,x| s + '/' + x  }
   end
   
-  def item_dl( item )
-    ret = nil
-    ch = item.children[0]
-    case ch.name
-    when /p/i
-      ch1 = item.children[1]
-      case ch1.name
-      when /DT/i
-#        puts "item_dl dt"        
-        ret = item_dt( ch1 )
-#        puts "ret=#{ret}"
-      else
-#        puts "item_dl else 2 #{ch1.name}"
-        ret = ch.text
-      end
-    else
-#      puts "item_dl else 1 #{ch.name}"
-    end
-#    puts "item_dl ret=#{ret}"
-    
-    ret
+  def get_dl_belong_to( item )
+    # ancestors()は昇順の配列で返すので、最初に見つかったdlが最も近いdlになる
+    item.ancestors().find{ |x| x.name == "dl" }
   end
 
-  def assign_name_to_ancesotr_h1(doc)
+  def assign_name_to_ancestor_h1(doc)
     x =doc.search("h1").first
     body_title = x.text.strip
     parent = x.parent
     
-    z = parent.children.find{ |x| x.name == "dl" }
-    @item_hs[z] = { :name => z.name, :title => body_title , :label => nil , :item => z }
+    dl = parent.children.find{ |x| x.name == "dl" }
+    register_hier_item( dl )
+    register_item( dl , body_title )
   end
-  
-  def assign_name_to_ancesotr_h3( doc )
+
+  def assign_name_to_ancestor_h3( doc , state = :second )
     doc.search( "h3" ).each do |x|
-      puts x.path
       text = x.text.strip
-      puts text
+      # parentはdt
+      parent = x.parent
+      if state != :second
+        register_hier_item( parent )
+        register_item( parent , text )
+      end
+
+      dl = get_dl_belong_to( parent )
+      if @item_hs[dl][:title] == nil
+        register_item( dl , @dl_hs[ dl.parent ][:text] ) if @dl_hs[ dl.parent ]
+        @dl_hs.delete(dl.parent)
+      elsif @item_hs[dl][:title].strip.size == 0
+        register_item( dl , @dl_hs[ dl.parent ][:text] ) if @dl_hs[ dl.parent ]
+        @dl_hs.delete(dl.parent)
+      else
+        if @dl_hs[dl] == nil
+          @dl_hs[ dl ] = { :child => parent, :text => text }
+        else
+          @dl_hs[ dl ][:text] = text
+        end
+      end
+    end
+  end
+
+  def assign_name_to_ancestor_a_stage1(doc )
+    doc.search( "a" ).each do |x|
+      text = x.text.strip
+      # parentはdt
+      parent = x.parent
+      register_hier_item( parent )
+      register_item( parent, text )
+    end
+  end
+
+  def assign_name_to_ancestor_a_stage2(doc )
+    doc.search( "a" ).each do |x|
+      text = x.text.strip
       # parentはdt
       parent = x.parent
 
-      get_top( parent )
-      idx = @tag_list.find_index{ |x|  x.name == "dt" }
-      # y はdtの１つ上(dl)
-      dt = @tag_list[ idx ]
-      y = @tag_list[ idx - 1 ]
-=begin
-      unless @item_hs[y]
-        @item_hs[y] = {
-          :name => y.name,
-          :title => nil ,
-          :label => nil ,
-          :item => y
-        }
+      dl = get_dl_belong_to( parent )
+      dl_parent = dl.parent
+      idx = dl_parent.children.index(dl)
+      # dlの直前のdtを得る
+      step = 1
+      while true
+        dt = dl_parent.children[ idx - step ]
+        break if dt.name == "dt"
+        step += 1
+        if idx < step
+          dt = nil
+          break
+        end
       end
-=end
-      @tag_list = []
-      get_top( y )
-      #      @item_hs[y][:tag_list] = @tag_list
-      p "=y.path"
-      puts y.path
-      p "=parent.path"
-      puts parent.path
-
-      puts "=y.children"
-      y.children.map{ |x| puts x.path }
-      puts "=========="
-      idz = y.children.find_index(dt)
-      puts "idz=#{idz}"
-      z = y.children[ idz + 1 ]
-      if @item_hs[z]
-        @item_hs[z][:title] = text unless @item_hs[z][:title]
-      else
-        @item_hs[z] = {
-          :name => z.name,
-          :title => text ,
-          :label => nil ,
-          :item => z
-        }
+      if @item_hs[dl] == nil
+        register_hier_item( dl )
+        register_item( dl , @item_hs[dt][:title] )
+      elsif @item_hs[dl][:title] == nil
+        @item_hs[dl][:title] = @item_hs[dt][:title]
       end
-      @tag_list = []
-      puts "-------------------"
     end
-  end
-  
-  def assign_name_to_ancesotr_a(doc )
-    doc.search( "a" ).each do |x|
-      text = x.text.strip
-
-      get_top( x.parent )
-      
-      # dt -> a
-      parent = x.parent
-      parent_ex = @item_hs[parent]
-      if parent_ex
-        parent_ex[:title] = text
-      else
-        @item_hs[parent] = { :name => parent.name, :title => text , :label => nil , :item => parent }
-        parent_ex = @item_hs[parent]
-      end
-      @item_hs[parent][:tag_list] = @tag_list
-=begin
-      idx = @tag_list.find_index{ |x|  x.name == "dt" }
-      y = @tag_list[ idx - 1 ]
-      @item_hs[y] = {
-        :name => y.name,
-        :title => text ,
-        :label => nil ,
-        :item => y
-      }
-      @tag_list = []
-      get_top( y )
-      @item_hs[y][:tag_list] = @tag_list
-=end
-      @tag_list = []
-    end
-  end
+  end  
 
   def listup_a(doc)
     doc.search("a").each do |t|
+      str = ""
       parent = t.parent
       list = @item_hs[parent][:hier]
-      unless list
-        tag_list = @item_hs[parent][:tag_list]
-        idx = tag_list.find_index{ |x| x.name == "dt" }
-#        @item_hs[parent][:hier] = make_hier( tag_list[0,(idx - 1)] )
-        @item_hs[parent][:hier] = make_hier( tag_list[0,(idx)] )        
+
+#      puts inspect_ancestors( t )
+      
+      if !list or list.size == 0
+        register_hier_item(parent)
+        list = @item_hs[parent][:hier]
       end
-      hier_str = @item_hs[parent][:hier].collect{ |x|  x[:title] }.join("/")
-      puts hier_str
-      puts @item_hs[parent][:title]
-      puts "-----"
+      if list.size > 0
+        idx = list.find_index{ |x| x[:name] == "dt" }
+        if idx 
+#          listx = list[0, idx - 1 ]
+          listx = list[0, idx ]
+          if listx
+            ax = listx.collect{ |y| y[:title] }
+            str = ax.reduce(""){ |s,x| s + '/' + ( x != nil ? x : "") }
+            puts "#{str},#{t.text.strip},#{t['href']}\n"
+          end
+        end
+      else
+        #
+      end
     end
   end
-  
-  def make_hier( tag_list )
-    tag_list.collect{ |x|
-      puts x.name
-      case x.name
-      when "dt"
-        @item_hs[x] = { :name => x.name, :title => nil , :label => nil , :item => x } unless @item_hs[x]
-      when "dl"
-        @item_hs[x] = { :name => x.name, :title => nil , :label => nil , :item => x } unless @item_hs[x]
-      else
-#        puts x.text
-      end
-      @item_hs[x]
-    }
-  end
 
-  def get_category_listy
+  def get_category_list
     File.open( @fname ){ |file|
       doc = Nokogiri::HTML::Document.parse(file)
-      assign_name_to_ancesotr_h1( doc )
-      assign_name_to_ancesotr_h3( doc )
-      assign_name_to_ancesotr_a( doc )
+      assign_name_to_ancestor_h1( doc )
+      assign_name_to_ancestor_h3( doc , :first )
+      assign_name_to_ancestor_a_stage1(doc )
+
+      assign_name_to_ancestor_h3( doc , :second )
+      assign_name_to_ancestor_a_stage2(doc )
+
+#      print_ancestor_of_h3( doc )
+
       listup_a(doc)
     }
   end
- 
-  def print_node_tree_sub( node )
-    if node
-      puts node.category + " in print_node_tree_sub"
-      print_node_tree_sub( node.child )
-      print_node_tree_sub( node.next )
+
+  def print_ancestor_of_h3( doc )
+    doc.search( "h3" ).each do |x|
+      puts inspect_ancestors( x )
     end
   end
-
-  def print_node_tree
-    puts "In print_node_tree"
-    print_node_tree_sub( @node_hs[:ROOT] )
-  end
-
 end
 
 fname = ARGV[0]
 
-#str = Pathname( fname ).expand_path.read
 c2l = Chbk2list.new( fname )
-#c2l.get_category_list
-#c2l.get_category_list2
-#c2l.get_category_list_h1
-#puts "next get_category_listx"
-#c2l.get_category_listx
-puts "next get_category_listy"
-c2l.get_category_listy
-#puts "next print_node_tree"
-
-#c21.print_node_tree
-#puts "x"
+c2l.get_category_list
